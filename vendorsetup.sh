525026f8dfbe87b5a0fd4c8fb90b75c11216b101
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2024-10-14 23:40:42 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,1 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo twrp_manaus-user
-add_lunch_combo twrp_manaus-userdebug
-add_lunch_combo twrp_manaus-eng
 export LC_ALL=C
