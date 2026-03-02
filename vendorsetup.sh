2c0a559db97c7941983a6b74ba2d087db45bc352
BlackNight Shadow <Blacknight5644bn@gmail.com>
2026-03-02 04:02:37 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,13 @@
-# Orangefox Recovery Project Configuration
+#!/bin/bash
+# OrangeFox Recovery Project Configuration
 
 export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_BUILD_DEVICE=<device>
+export FOX_BUILD_DEVICE=manaus
 export LC_ALL="C"
 export OF_FORCE_PREBUILT_KERNEL=1
 export FOX_AB_DEVICE=1
+export FOX_VIRTUAL_AB_DEVICE=1
 export FOX_VANILLA_BUILD=1
-export OF_MAINTAINER = BN_LOS
+export OF_MAINTAINER=BN_LOS
 export TARGET_ARCH=arm64
-export FOX_ENABLE_KERNELSU_NEXT_SUPPORT = 1
+export FOX_ENABLE_KERNELSU_NEXT_SUPPORT=1
